b35acf6bc16e89781092c4353fdb1946150cdd97
Lena Felica <leonatdg@mail.ru>
2023-07-04 12:28:29 +0300

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -3,7 +3,6 @@ import { useTelegram } from "../../hooks/useTelegram";
 import ProductItem from "../ProductItem/ProductItem";
 import './ProductList.css';
 
-//* массив продуктов:
 const products = [
    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
@@ -15,9 +14,6 @@ const products = [
    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
 ]
 
-//* Функция дя подсчета общей стоимости:
-//* редьюс аккмулирует на каждой итерации какой-то результат - кладывает
-//* получаем общую стоимоть
 const getTotalPrice = (items = []) => {
    return items.reduce((acc, item) => {
        return acc += item.price
@@ -25,45 +21,31 @@ const getTotalPrice = (items = []) => {
 }
 
 const ProductList = () => {
-   const[addedItems, setAddedItems] = useState([]); // состояние, в которое сохраняем добавленные продукты - корзина - массив с объектами
-//* с помощью useTelegram хука получаем наш объект = tg
+   const[addedItems, setAddedItems] = useState([]); 
    const {tg} = useTelegram();   
-//* Функция добавления товара или даления из корзины:
-//* в ProductItem мы этот продукт наверх рокидываем(в функции колбэк)
-// const onAddHandler = () => {
-//    onAdd(product)
-// }
-//* а здесь его принимаем:
+
 const onAdd = (product) => {
-   //* находим по id товар в корзине
     const alreadyAdded = addedItems.find(item => item.id === product.id)
     let newItems = [];
-//* если он уже сть, то мы его оттуда удаляем!!!
-//* else - если его там нет, то в конец этой корзины товар добавем(развернув при этом все, что там уже были!!)
+
     if(alreadyAdded) {
       newItems = addedItems.filter(item => item.id !== product.id)
     } else {
       newItems = [...addedItems, product]
     }
-//* изменяем состояние на тот массив, который в результате получился!!    
+ 
     setAddedItems(newItems);
 
-//* теперь надо показать главную кнопку - то мы здесь делаем:
-//* провряем - если корзина пуста, то мы эту кнопку скрываем
-//* то есть, если все товар из козины удалили - кнопку н поазываем 
-//* в обратном случае мы кнопку показываем!
-//* если в орзине есть хоть что-то, то показываем!!
    if(newItems.length === 0) {
       tg.MainButton.hide();
    } else {
       tg.MainButton.show();
-//* на кнопку добавляем общую стоимость товаров, на которую мы накликали
       tg.MainButton.setParams({
          text: `Купить ${getTotalPrice(newItems)}`
       })
    }
 
-   }
+}
 
    return (
       <div className={'list'}>
